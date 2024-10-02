859a4bc7bfb209122a16b2ed6d7b0930ed3c7f98
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2024-10-02 23:13:33 +0600

    Create Contains-Duplicate.js

---
+++ DSA/NeetCode/Contains-Duplicate.js
@@ -0,0 +1,31 @@
+/* https://neetcode.io/problems/duplicate-integer */
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        
+        let firstIndex = 0;
+        let lastIndex = nums.length - 1;
+
+        while(lastIndex > firstIndex)
+        {
+            if(nums[lastIndex] === nums[firstIndex])
+            {
+                return true;
+            }
+            
+            lastIndex--;
+
+            if(lastIndex === firstIndex)
+            {
+                firstIndex++;
+                lastIndex = nums.length - 1;
+            }
+        }
+
+        return false;
+    }
+}
